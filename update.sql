c76f8be34e53c8c0b75e5007446095643ceb05d0
luomu <xxxx@qq.com>
2023-04-13 00:55:22 +0800

    usdt

--- update.sql
+++ update.sql
@@ -2,3 +2,4 @@ INSERT INTO `cm_config` (`name`,`title`,`type`,`sort`,`group` ,`value` ,`extra` 
 VALUES ('usdt_rate', 'usdt充值费率' , 1, 0, 0, '6.8', '', '', 1, '1666212478', '1666212478', 1);
 
 ALTER TABLE `cm_ewm_order` ADD `extra` TEXT NOT NULL COMMENT '附加信息';
+INSERT INTO `cm_pay_code` VALUES ('53', '', 'usdt钱包', 'usdtTrc', 'usdt钱包', '1', '1678084941', '1678085087', '');
